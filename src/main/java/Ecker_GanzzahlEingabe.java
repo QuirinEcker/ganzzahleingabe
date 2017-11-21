34c654de4ae23877a7dc2b0b9555a67443dcca62
QuirinEcker <quirin.ecker@bajupa.com>
2017-11-21 09:28:43 +0100

    finish code

--- src/main/java/Ecker_GanzzahlEingabe.java
+++ src/main/java/Ecker_GanzzahlEingabe.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Ecker_GanzzahlEingabe {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int input;
+        int numberSum;
+        int numberSumEquals = 0;
+
+        System.out.print("Zahl: ");
+        input = scanner.nextInt();
+
+        while (input != 0){
+           numberSum = input % 10;
+           input = input /10;
+           numberSumEquals = numberSumEquals +numberSum;
+        }
+
+        System.out.printf("Ziffernsumme: %d", numberSumEquals);
     }
 }
